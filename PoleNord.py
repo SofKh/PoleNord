121e17f22b27c52f2551f49218fa0e4e3002591b
SofKh <sofiane.chkh@gmail.com>
2022-03-01 11:46:21 -0500

    Distance entre deux points

--- PoleNord.py
+++ PoleNord.py
@@ -1,9 +1,13 @@
 from lib2to3.pytree import convert
 
-
+import math
 def dms_to_dd(degres, minutes, secondes):
     Convert = degres + minutes/60 + secondes/3600
     return convert
 
+
+def distance_deux_points(point1, point2, point3, point4):
+    Formule_distance = math.sqrt((point2-point1)**2 + (point4-point3)**2)
+    return Formule_distance
 
 
