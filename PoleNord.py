05c35bc9b247fc0330c514159b6fa972bae5adfd
SofKh <sofiane.chkh@gmail.com>
2022-03-01 12:00:40 -0500

    IMPLEMENTATION DES VALEURS

--- PoleNord.py
+++ PoleNord.py
@@ -10,4 +10,16 @@ def distance_deux_points(point1, point2, point3, point4):
     Formule_distance = math.sqrt((point2-point1)**2 + (point4-point3)**2)
     return Formule_distance
 
+POLE_NORD_X = 86, 0, 0
+POLE_NORD_Y = 172, 0, 0
+MONTREAL_X = 45, 30, 31
+MONTREAL_Y = 73, 35, 16
+
+polenord_dmsX = dms_to_dd(POLE_NORD_X)
+polenord_dmsY = dms_to_dd(POLE_NORD_Y)
+montreal_dmsX = dms_to_dd(MONTREAL_X)
+montreal_dmsY = dms_to_dd(MONTREAL_Y)
+
+
+
 
