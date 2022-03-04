b180a6690b11a6c246ba57764147ac04c4dbd0da
SofKh <sofiane.chkh@gmail.com>
2022-03-04 12:35:44 -0500

    ajout de commentaire dans le code pour finaliser

--- PoleNord.py
+++ PoleNord.py
@@ -1,14 +1,20 @@
 
+# Importation de la formule (distance entre 2 points utilisée par la suite de l'exercice)
+
 import math
 
+# créer la fonction pour obtenir la conversion de la position Dms en Dd
 def dms_to_dd(position):
     degres, minutes, secondes = position
     return degres + minutes / 60 + secondes / 3600
 
+# Créer la fonction pour obtenir la distance entre deux points
 
 def distance_deux_points(point1, point2, point3, point4):
     Formule_distance = math.sqrt((point2-point1)**2 + (point4-point3)**2)
     return Formule_distance
+
+# Exemple avec la ville de Montreal
 
 POLE_NORD_X = 86, 29, 38
 POLE_NORD_Y = 172, 52, 1
